a83b6c1f65e72f5bd277b43e69e4f1eb6048ce0c
Ponchisao326 <ponchisao326@hotmail.com>
2025-09-30 10:38:05 +0200

    Initial commit

---
+++ src/TypeScript/Icons.ts
@@ -0,0 +1,14 @@
+import { createIcons, Skull, Settings, Download, FolderOpen, LogIn, Swords, Heart, Zap } from 'lucide';
+
+createIcons({
+  icons: {
+      Skull,
+      Settings,
+      Download,
+      FolderOpen,
+      LogIn,
+      Swords,
+      Heart,
+      Zap
+  }
+});
