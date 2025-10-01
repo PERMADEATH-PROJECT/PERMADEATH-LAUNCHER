665399786d8d8eee1adef6d878fa80d88ea10734
ponchisao326_ <ponchisao326@hotmail.com>
2025-10-02 01:12:48 +0200

    untabbed

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,35 @@
 import { defineConfig } from "vite";
 import { resolve } from 'path';
 
-    const host = process.env.TAURI_DEV_HOST;
+const host = process.env.TAURI_DEV_HOST;
 
     // https://vite.dev/config/
-    export default defineConfig(async () => {
-      return {
-        clearScreen: false,
-        server: {
-          port: 1420,
-          strictPort: true,
-          host: host || false,
-          hmr: host
-            ? {
-                protocol: "ws",
-                host,
-                port: 1421,
-              }
-            : undefined,
-          watch: {
-            ignored: ["**/src-tauri/**"],
-          },
-        },
-        publicDir: "public",
-        build: {
-          cssCodeSplit: false,
-          rollupOptions: {
-            input: {
-              main: resolve(__dirname, 'index.html')
-            }
+export default defineConfig(async () => {
+  return {
+    clearScreen: false,
+    server: {
+      port: 1420,
+      strictPort: true,
+      host: host || false,
+      hmr: host
+        ? {
+            protocol: "ws",
+            host,
+            port: 1421,
           }
+        : undefined,
+      watch: {
+        ignored: ["**/src-tauri/**"],
+      },
+    },
+    publicDir: "public",
+    build: {
+      cssCodeSplit: false,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html')
         }
-      };
-    });
+      }
+    }
+  };
+});
